13d87407b4f33752a89c17c360ac72be2b0871cd
A01709167 <A01709167@tec.mx>
2025-03-24 13:31:09 -0600

    Hellow

--- singleelements.js
+++ singleelements.js
@@ -1,10 +1,12 @@
-// Navigation bar 
+// script.js
 const dropdownBtn = document.querySelectorAll(".dropdown-btn");
 const dropdown = document.querySelectorAll(".dropdown");
 const hamburgerBtn = document.getElementById("hamburger");
 const navMenu = document.querySelector(".menu");
 const links = document.querySelectorAll(".dropdown a");
 
+
+//Swear to explain later
 function setAriaExpandedFalse() {
     dropdownBtn.forEach((btn) => btn.setAttribute("aria-expanded", "false"));
   }
@@ -32,6 +34,8 @@ function setAriaExpandedFalse() {
     });
   });
 
+  //Dinamically opens the dropdown only after clicking it. In addition to toggling the dropdown menu, we added a condition to check if the current dropdown element id matches with the active button. This makes sure only one dropdown element is expanded at a time.
+
   dropdownBtn.forEach((btn) => {
     btn.addEventListener("click", function (e) {
       const dropdownIndex = e.currentTarget.dataset.dropdown;
@@ -46,6 +50,8 @@ function setAriaExpandedFalse() {
       e.stopPropagation();
     });
   });
+
+  //Open the actual dropdown
   btn.setAttribute(
     "aria-expanded",
     btn.getAttribute("aria-expanded") === "false" ? "true" : "false"
@@ -64,7 +70,7 @@ links.forEach((link) =>
     closeDropdownMenu();
     setAriaExpandedFalse();
   });
-  
+
   // close dropdown when the escape key is pressed
   document.addEventListener("keydown", (e) => {
     if (e.key === "Escape") {
@@ -73,8 +79,15 @@ links.forEach((link) =>
     }
   });
 
-  //toggle aria expanded property
-  btn.setAttribute(
-    "aria-expanded",
-    btn.getAttribute("aria-expanded") === "false" ? "true" : "false"
-);
+  //Toggle hamburguer button
+  links.forEach((link) =>
+    link.addEventListener("click", () => {
+      closeDropdownMenu();
+      setAriaExpandedFalse();
+      toggleHamburger();
+    })
+  );
+
+  //something something updates the class to make it show
+
+  hamburgerBtn.addEventListener("click", toggleHamburger);
